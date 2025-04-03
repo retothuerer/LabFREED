ed787ea43cdc4ceb427b826ef6b31b2ca305c831
retothuerer <94900286+retothuerer@users.noreply.github.com>
2025-04-03 07:12:04 +0200

    trex tests rename to parse

---
+++ tests/test_TREXExtension/test_TREX_parse.py
@@ -0,0 +1,215 @@
+import re
+import pytest
+
+from labfreed.TREX.data_model import TREX
+from labfreed.TREX.parse import _from_trex_string, TREX_Parser
+
+from labfreed.validation import LabFREEDValidationError
+
+parser = TREX_Parser()
+
+    
+def test_trex_parse():
+    trex_str = 'SUM$TREX/A$T.A:ASDFAS+B$T.B:T'
+    trex = parser.parse_trex_str(trex_str)
+    assert trex.name == 'SUM'
+    assert trex.type == 'TREX'
+    seg = trex.segments
+    assert trex.get_segment('A').type == 'T.A'
+    assert trex.get_segment('A').value == 'ASDFAS'
+    assert trex.get_segment('B').type == 'T.B'
+    assert trex.get_segment('B').value == 'T'
+    
+    
+    
+    
+# Segment Key   
+def test_invalid_keys():
+    trex_str = '£TIM$HUR:1'
+    with pytest.raises(LabFREEDValidationError) as e:
+        trex = parser.parse_trex_str(trex_str, name="A")
+        
+    trex_str = 'tImE$HUR:1'
+    with pytest.raises(LabFREEDValidationError) as e:
+        trex = parser.parse_trex_str(trex_str, name="A")
+
+
+# Numeric Segment
+def test_numeric_segment_valid():
+    trex_str ='TIME$HUR:25.0'
+    trex = parser.parse_trex_str(trex_str, name="A")
+    assert trex.data == trex_str
+    
+    trex_str ='TIME$HUR:25'
+    trex = parser.parse_trex_str(trex_str, name="A")
+    assert trex.data == trex_str
+    
+    trex_str ='TIME$HUR:-25.0'
+    trex = parser.parse_trex_str(trex_str, name="A")
+    assert trex.data == trex_str
+    
+    trex_str ='TIME$HUR:-25'
+    trex = parser.parse_trex_str(trex_str, name="A")
+    assert trex.data == trex_str
+    
+def test_numeric_segment_invalid_values():
+    with pytest.raises(ValueError):
+        trex_str ='TIME$HUR:A'
+        trex = parser.parse_trex_str(trex_str, name="A")
+        assert trex.data == trex_str
+    
+    with pytest.raises(ValueError):
+        trex_str ='TIME$HUR:1.1.1'
+        trex = parser.parse_trex_str(trex_str, name="A")
+        assert trex.data == trex_str
+        
+def test_numeric_segment_invalid_unit():
+    with pytest.raises(ValueError):
+        trex_str ='TIME$HIP:A'
+        trex = parser.parse_trex_str(trex_str, name="A")
+        assert trex.data == trex_str
+    
+        
+        
+# DateSegments
+def test_valid_dates():
+    dates = [
+        '20240305'
+    ]
+    for e in dates:
+        trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+
+def test_valid_times():
+    times = [
+        'T0102', 'T010203', 'T010203.456'
+    ]
+    for e in times:
+        trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+    
+def test_valid_datetimes():
+    datetimes = [
+        '20240305T0102', '20240305T010203', '20240305T010203', '20240305T010203.456'
+    ]
+    for e in datetimes:
+        trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_dates_format():
+    dates = [
+        '2024030'
+    ]
+    for e in dates:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+            
+            
+def test_invalid_dates():
+    dates = [
+        '20241331', '20240000'
+    ]
+    for e in dates:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+            
+            
+def test_invalid_times_format():
+    times = [
+        'T2561'
+    ]
+    for e in times:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.D:{e}', name='A')
+    
+
+
+# BoolSgments
+def test_valid_bool():
+    b = [
+        'T', 'F'
+    ]
+    for e in b:
+        trex = parser.parse_trex_str(f'A$T.B:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_bool():
+    b = [
+        '1', '0', 'TRUE', 'FALSE'
+    ]
+    for e in b:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.B:{e}', name='A')
+            
+            
+            
+# Alphanumeric Segment
+def test_valid_alphanumeric():
+    b = [
+        'ABCDEFGHIJKLMNOPQRSTUVW012345678.-', 
+    ]
+    for e in b:
+        trex = parser.parse_trex_str(f'A$T.A:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_alphanumeric():
+    b = [
+        'a', '£', '<', 'ABCDeFGh'
+    ]
+    for e in b:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.A:{e}', name='A')
+
+
+# Text Segment
+def test_valid_text():
+    b = [
+        'ABCDEFGHIJKLMNOPQRSTUVW012345678', 
+    ]
+    for e in b:
+        trex = parser.parse_trex_str(f'A$T.T:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_text():
+    b = [
+        'a', '£', '<', 'ABCDeFGh', 'ABCDEFGHIJKLMNOPQRSTUVW012345678.-'
+    ]
+    for e in b:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.T:{e}', name='A')
+
+      
+# Binary Segment
+def test_valid_binary():
+    b = [
+        'ABCDEFGHIJKLMNOPQRSTUVW012345678', 
+    ]
+    for e in b:
+        trex = parser.parse_trex_str(f'A$T.X:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_binary():
+    b = [
+        'a', '£', '<', 'ABCDeFGh', 'ABCDEFGHIJKLMNOPQRSTUVW012345678.-'
+    ]
+    for e in b:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$T.X:{e}', name='A')
+            
+            
+# Error Segment
+def test_valid_error():
+    b = [
+        'ABCDEFGHIJKLMNOPQRSTUVW012345678.-', 
+    ]
+    for e in b:
+        trex = parser.parse_trex_str(f'A$E:{e}', name='A')
+        assert not trex.get_nested_validation_messages()
+        
+def test_invalid_error():
+    b = [
+        'a', '£', '<', 'ABCDeFGh'
+    ]
+    for e in b:
+        with pytest.raises(ValueError):
+            trex = parser.parse_trex_str(f'A$E:{e}', name='A')
