1dbdb994ec8d1d866f86f3d219123408683fbf11
retothuerer <94900286+retothuerer@users.noreply.github.com>
2025-04-03 07:13:31 +0200

    modified pac testsfor new validation mechanism,

--- tests/test_PAC_ID/test_pac_id_parse.py
+++ tests/test_PAC_ID/test_pac_id_parse.py
@@ -14,20 +14,20 @@ parser = PAC_Parser()
 
 # Issuer
 def test_standard_base_gives_correct_issuer():
-    pac, _ = parser.parse_pac_url("HTTPS://PAC.METTORIUS.COM/" + valid_standard_segments)
+    pac = parser.parse_pac_url("HTTPS://PAC.METTORIUS.COM/" + valid_standard_segments)
     assert pac.pac_id.issuer == "METTORIUS.COM"
        
 def test_pac_can_be_missing_from_domain():
-    pac, _ = parser.parse_pac_url("HTTPS://METTORIUS.COM/" + valid_standard_segments)
+    pac = parser.parse_pac_url("HTTPS://METTORIUS.COM/" + valid_standard_segments)
     assert pac.pac_id.issuer == "METTORIUS.COM"
      
 def test_pac_can_be_missing_from_domain():
-    pac, _ = parser.parse_pac_url("METTORIUS.COM/" + valid_standard_segments)
+    pac = parser.parse_pac_url("METTORIUS.COM/" + valid_standard_segments)
     assert pac.pac_id.issuer == "METTORIUS.COM"
     
 def test_issuer_must_be_valid_domain():
     with pytest.raises(Exception):
-        pac, _ = parser.parse_pac_url("HTTPS://METTORIUS/" + valid_standard_segments)
+        pac = parser.parse_pac_url("HTTPS://METTORIUS/" + valid_standard_segments)
         
         
         
@@ -39,19 +39,19 @@ def test_pac_must_have_at_least_one_segment():
         pac = parser.parse_pac_url(valid_base = "").pac_id
           
 def test_identifier_named_segment():
-    pac, _ = parser.parse_pac_url(valid_base + "KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "KEY:VAL")
     seg: IDSegment = pac.pac_id.identifier.segments[0]
     assert seg.key == "KEY"
     assert seg.value == "VAL"
     
 def test_identifier_unnamed_segment():
-    pac, _ = parser.parse_pac_url(valid_base + "VAL")
+    pac = parser.parse_pac_url(valid_base + "VAL")
     seg: IDSegment = pac.pac_id.identifier.segments[0]
     assert not seg.key
     assert seg.value == "VAL"
      
 def test_identifier_combination_of_named_and_unnamed_segments():
-    pac, _ = parser.parse_pac_url(valid_base + "KEY0:VAL0/VAL1/KEY2:VAL2")
+    pac = parser.parse_pac_url(valid_base + "KEY0:VAL0/VAL1/KEY2:VAL2")
     pac = pac.pac_id
     seg: IDSegment = pac.identifier.segments[0]
     assert seg.key == 'KEY0'
@@ -67,10 +67,10 @@ def test_identifier_combination_of_named_and_unnamed_segments():
       
 def test_keys_must_be_unique():
     with pytest.raises(Exception):
-        pac, _ = parser.parse_pac_url(valid_base + "KEY:VAL/KEY:ANOTHERVAL/KEY:VAL/KEY2:ANOTHERVAL")
+        pac = parser.parse_pac_url(valid_base + "KEY:VAL/KEY:ANOTHERVAL/KEY:VAL/KEY2:ANOTHERVAL")
         
 def test_keys_can_repeat_accross_categories():
-    pac, _ = parser.parse_pac_url(valid_base + "-MD/KEY:VAL/-MS/KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "-MD/KEY:VAL/-MS/KEY:VAL")
     assert True # made it here without exception > it's fine
         
         
@@ -78,27 +78,27 @@ def test_keys_can_repeat_accross_categories():
 
 # Identifier Segments Categories (Recommendation)
 def test_basic_valid_category():
-    pac, _ = parser.parse_pac_url(valid_base + "-MD/KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "-MD/KEY:VAL")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.key == '-MD'
     assert cat.segments[0].key == 'KEY'
     assert cat.segments[0].value == 'VAL'
     
 def test_if_no_category_is_specified_default_to_unnamed_category():
-    pac, _ = parser.parse_pac_url(valid_base + "KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "KEY:VAL")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.key == None
     assert cat.segments[0].key == 'KEY'
     assert cat.segments[0].value == 'VAL'
     
 def test_missing_dash_in_category_name_is_interpreted_as_unnamed_segment():
-    pac, _ = parser.parse_pac_url(valid_base + "MD/KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "MD/KEY:VAL")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.key == None
     assert cat.segments[0].value == 'MD'
     
 def test_category_with_multiple_segments():
-    pac, _ = parser.parse_pac_url(valid_base + "-MD/KEY0:VAL0/VAL1/KEY2:VAL2")
+    pac = parser.parse_pac_url(valid_base + "-MD/KEY0:VAL0/VAL1/KEY2:VAL2")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.key == '-MD'
     
@@ -115,7 +115,7 @@ def test_category_with_multiple_segments():
     assert seg.value == "VAL2"
       
 def test_two_categories():
-    pac, _ = parser.parse_pac_url(valid_base + "-DR/KEY:VAL/-MD/KEY:VAL")
+    pac = parser.parse_pac_url(valid_base + "-DR/KEY:VAL/-MD/KEY:VAL")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.key == '-DR'
     assert cat.segments[0].key == 'KEY'
@@ -127,7 +127,7 @@ def test_two_categories():
     assert cat.segments[0].value == 'VAL'
      
 def test_three_categories():
-    pac, _ = parser.parse_pac_url(valid_base + "-DR/KEY0:VAL0/-MD/KEY1:VAL1/-CAT/KEY2:VAL2")
+    pac = parser.parse_pac_url(valid_base + "-DR/KEY0:VAL0/-MD/KEY1:VAL1/-CAT/KEY2:VAL2")
     pac = pac.pac_id
     cat: Category = pac.identifier.categories[0]
     assert cat.key == '-DR'
@@ -145,13 +145,13 @@ def test_three_categories():
     assert cat.segments[0].value == 'VAL2'
     
 def test_implied_segments_of_MD_category():
-    pac, _ = parser.parse_pac_url(valid_base + "-MD/0/1")
+    pac = parser.parse_pac_url(valid_base + "-MD/0/1")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[0].key == '240'
     assert cat.segments[1].key == '21'
     
 def test_implied_segments_of_MS_category():
-    pac, _ = parser.parse_pac_url(valid_base + "-MS/0/1/2/3/4")
+    pac = parser.parse_pac_url(valid_base + "-MS/0/1/2/3/4")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[0].key == '240'
     assert cat.segments[1].key == '10'
@@ -160,7 +160,7 @@ def test_implied_segments_of_MS_category():
     assert cat.segments[4].key == '250'
     
 def test_implied_segments_of_MC_category():
-    pac, _ = parser.parse_pac_url(valid_base + "-MC/0/1/2/3/4")
+    pac = parser.parse_pac_url(valid_base + "-MC/0/1/2/3/4")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[0].key == '240'
     assert cat.segments[1].key == '10'
@@ -169,7 +169,7 @@ def test_implied_segments_of_MC_category():
     assert cat.segments[4].key == '250'
     
 def test_implied_segments_of_MM_category():
-    pac, _ = parser.parse_pac_url(valid_base + "-MM/0/1/2/3/4")
+    pac = parser.parse_pac_url(valid_base + "-MM/0/1/2/3/4")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[0].key == '240'
     assert cat.segments[1].key == '10'
@@ -178,7 +178,7 @@ def test_implied_segments_of_MM_category():
     assert cat.segments[4].key == '250'
     
 def test_stop_implying_segments_after_an_explicit_one_is_found():
-    pac, _ = parser.parse_pac_url(valid_base + "-MS/0/1/KEY:2/3/4")
+    pac = parser.parse_pac_url(valid_base + "-MS/0/1/KEY:2/3/4")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[0].key == '240'
     assert cat.segments[1].key == '10'
@@ -187,7 +187,7 @@ def test_stop_implying_segments_after_an_explicit_one_is_found():
     assert cat.segments[4].key == None
     
 def test_more_segments_than_implicit_keys():
-    pac, _ = parser.parse_pac_url(valid_base + "-MD/IMPLIED1/IMPLIED2/ADDITIONAL")
+    pac = parser.parse_pac_url(valid_base + "-MD/IMPLIED1/IMPLIED2/ADDITIONAL")
     cat: Category = pac.pac_id.identifier.categories[0]
     assert cat.segments[2].key == None
     
@@ -195,7 +195,7 @@ def test_more_segments_than_implicit_keys():
     
 # Extensions
 def test_valid_extensions():
-    pac, _ = parser.parse_pac_url(valid_base + valid_standard_segments + "*name1$t1/data1*name2$t2/data2")
+    pac = parser.parse_pac_url(valid_base + valid_standard_segments + "*name1$t1/data1*name2$t2/data2")
     extensions = pac.extensions
     ext: Extension = extensions[0]
     assert ext.name == 'name1'
@@ -229,7 +229,7 @@ def test_known_extension_types_are_parsed():
             'KNOWN_EXTENSION': ExtensionMockType,
     }  
     parser_with_known_extension = PAC_Parser(extension_interpreters)
-    pac, _ = parser_with_known_extension.parse_pac_url(valid_base + valid_standard_segments + "*name1$KNOWN_EXTENSION/data1")
+    pac = parser_with_known_extension.parse_pac_url(valid_base + valid_standard_segments + "*name1$KNOWN_EXTENSION/data1")
     extensions= pac.extensions
     ext: Extension = extensions[0]
     assert isinstance(ext, ExtensionMockType)
@@ -239,7 +239,7 @@ def test_known_extension_types_are_parsed():
     
     
 def test_imply_display_name_and_summary_extension():
-    pac, _ = parser.parse_pac_url(valid_base + valid_standard_segments + "*data1*data2")
+    pac = parser.parse_pac_url(valid_base + valid_standard_segments + "*data1*data2")
     extensions = pac.extensions
     ext: Extension = extensions[0]
     assert ext.name == 'N'
@@ -251,7 +251,7 @@ def test_imply_display_name_and_summary_extension():
     
 def test_stop_imply_extensions_after_explicit():
     with pytest.raises(Exception):
-        pac, _ = parser.parse_pac_url(valid_base + valid_standard_segments + "*N$T/data1*data2")
+        pac = parser.parse_pac_url(valid_base + valid_standard_segments + "*N$T/data1*data2")
         pac.extensions
         
 
