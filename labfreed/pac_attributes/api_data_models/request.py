a5d1e8faa463d33fc4dbc36388678f0822e6416a
retothuerer <94900286+retothuerer@users.noreply.github.com>
2025-12-04 07:36:58 +0100

    request payload reduced to only one pac-id

--- labfreed/pac_attributes/api_data_models/request.py
+++ labfreed/pac_attributes/api_data_models/request.py
@@ -1,15 +1,17 @@
+import logging
 from typing import   Self
+from flask import redirect
 from pydantic import ConfigDict, model_validator
 from labfreed.labfreed_infrastructure import LabFREED_BaseModel, LabFREED_ValidationError, ValidationMsgLevel
 from labfreed.pac_id.pac_id import PAC_ID
 
 
-class AttributeRequestPayload(LabFREED_BaseModel):
+class AttributeRequestData(LabFREED_BaseModel):
     model_config = ConfigDict(frozen=True)
     
-    pac_ids: list[str]
-    language_preferences: list[str]|None = None
-    restrict_to_attribute_groups: list[str]|None = None
+    pac_id: str
+    language_preferences: list[str]|str|None = None
+    restrict_to_attribute_groups: list[str]|str|None = None
     suppress_forward_lookup: bool = False
     
     def as_json(self):
@@ -21,30 +23,23 @@ class AttributeRequestPayload(LabFREED_BaseModel):
     
     @model_validator(mode="before")
     @classmethod
-    def _handle_single_pac_url(cls, data):
-        p = data.get('pac_urls')
-        if isinstance(p, str):
-            data['pac_urls'] = [p]
+    def _handle_multiple_pac_url(cls, data):
+        p = data.get('pac_id')
+        if isinstance(p, list) and len(p) > 0:
+            logging.error("Multiple PAC-ID are no longer supported. For half assed backwards compatibility the first element is now selected.")
+            data['pac_id'] = p[0]
         return data
     
     @model_validator(mode="after")
-    def _validate_pacs(self) -> Self:
-        if len(self.pac_ids) > 100:
+    def _validate_pacs(self) -> Self:           
+        try:
+            PAC_ID.from_url(self.pac_id)
+        except LabFREED_ValidationError:
             self._add_validation_message(
-                        source="pacs",
-                        level = ValidationMsgLevel.ERROR,
-                        msg='The number of pac-ids must be limited to 100'
-                    )
-            
-        for pac_url in self.pac_ids:
-            try:
-                PAC_ID.from_url(pac_url)
-            except LabFREED_ValidationError:
-                self._add_validation_message(
-                        source="pacs",
-                        level = ValidationMsgLevel.ERROR,
-                        msg='{pac_url} is not a valid PAC-ID'
-                    )
+                    source="pac_id",
+                    level = ValidationMsgLevel.ERROR,
+                    msg='{self.pac_id} is not a valid PAC-ID'
+                )
                 
         if not self.is_valid:
             raise LabFREED_ValidationError(message='Invalid request', validation_msgs=self.validation_messages())
