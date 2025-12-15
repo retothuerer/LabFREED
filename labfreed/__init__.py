5efcc5ddc8456c10e2be570985bf8f9f60fa1ae6
retothuerer <94900286+retothuerer@users.noreply.github.com>
2025-12-15 15:51:00 +0100

    new version with favicon for landing page

--- labfreed/__init__.py
+++ labfreed/__init__.py
@@ -2,7 +2,7 @@
 Python implementation of LabFREED building blocks
 '''
 
-__version__ = "1.0.0a24"
+__version__ = "1.0.0a25"
 
 from labfreed.pac_id import *  # noqa: F403
 from labfreed.pac_cat import * # noqa: F403
