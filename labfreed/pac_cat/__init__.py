3c7b76d740654a1ba91fe5f4e53a4e9f990f7aae
retothuerer <94900286+retothuerer@users.noreply.github.com>
2026-07-27 09:50:30 -0700

    fix decalration of __all__ for docs

--- labfreed/pac_cat/__init__.py
+++ labfreed/pac_cat/__init__.py
@@ -1,8 +1,9 @@
 from .pac_cat import PAC_CAT
 from .category_base import Category
 from .predefined_categories import (
-    Material_Device, Material_Substance, Material_Consumable, Material_Misc, Data_Method, Data_Result, Data_Progress, 
-    Data_Calibration, Data_Abstract, category_key_to_class_map  # noqa: F401
+    Material_Device, Material_Substance, Material_Consumable, Material_Misc, Data_Method, Data_Result, Data_Progress,
+    Data_Calibration, Data_Static, Data_Misc, Processor_Software, Processor_Misc, Misc,
+    category_key_to_class_map  # noqa: F401
     )
 
 __all__ = [
@@ -16,5 +17,9 @@ __all__ = [
     "Data_Result",
     "Data_Progress",
     "Data_Calibration",
-    "Data_Abstract"
+    "Data_Static",
+    "Data_Misc",
+    "Processor_Software",
+    "Processor_Misc",
+    "Misc",
 ]
