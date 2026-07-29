70d19ad704bf5f74b497c2041433508af4ad297f
retothuerer <94900286+retothuerer@users.noreply.github.com>
2026-07-29 06:55:05 -0700

    well known attributes from apini

--- labfreed/pac_attributes/well_known_attribute_keys.py
+++ labfreed/pac_attributes/well_known_attribute_keys.py
@@ -16,7 +16,25 @@ class MetaAttributeKeys(Enum):
 
 
 class PhysicoChemicalProperties(Enum):
-    BOILINGPOINT    = "https://labfreed.org/dummy/boilingpoint"
-    MELTINGPOINT    = "https://labfreed.org/dummy/meltingpoint"
+    BOILINGPOINT    = "https://qudt.org/vocab/quantitykind/BoilingPoint"
+    MELTINGPOINT    = "https://qudt.org/vocab/quantitykind/MeltingPoint"
     POLARITY        = "https://labfreed.org/dummy/polarity"
-    DENSITY         = "https://labfreed.org/dummy/density"
+    DENSITY         = "https://qudt.org/vocab/quantitykind/Density"
+    MOLARMASS       = "https://qudt.org/vocab/quantitykind/MolarMass"
+    FLASHPOINT      = "https://qudt.org/vocab/quantitykind/FlashPoint"
+
+
+class ChemicalIdentifiers(Enum):
+    CAS_NUMBER          = "https://registry.identifiers.org/registry/cas"
+    EC_NUMBER           = "https://www.wikidata.org/wiki/Property:P232"
+    EMPIRICAL_FORMULA   = "https://w3id.org/chemrof/generalized_empirical_formula"
+
+
+class GuaranteeAnalysisProperties(Enum):
+    ASSAY           = "https://doi.org/10.1351/goldbook.08014"
+    WATER_CONTENT   = "https://identifiers.org/CHEBI:15377"
+
+
+class DocumentKeys(Enum):
+    DATASHEET           = "https://www.wikidata.org/wiki/Q20819677"
+    SAFETY_DATA_SHEET   = "https://www.wikidata.org/wiki/Q222067"
