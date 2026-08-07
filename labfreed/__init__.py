3a3f63cbca9d2620ebce153f5861702f953eb4fa
retothuerer <94900286+retothuerer@users.noreply.github.com>
2026-08-07 07:23:07 -0700

    new version

--- labfreed/__init__.py
+++ labfreed/__init__.py
@@ -2,7 +2,7 @@
 Python implementation of LabFREED building blocks
 '''
 
-__version__ = "1.0.0b46"
+__version__ = "1.0.0b47"
 
 from labfreed.pac_id import *  # noqa: F403
 from labfreed.pac_cat import * # noqa: F403
